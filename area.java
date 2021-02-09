bf0d044459e0d802ebfbd0b3579121aab42dff88
Vivekrawat101 <vaseervr@gmail.com>
2021-02-09 20:07:22 +0530

    comments are added to the program for simplified approach and for screen reading

--- area.java
+++ area.java
@@ -1,20 +1,21 @@
-class calculate{
+class calculate{ // object class
     public int side;
     
-    public calculate(int side){
+    public calculate(int side){ // parameter initialised to side of the square
         this.side = side;
     }
     
    public  void areaOfSquare(){
-       System.out.println("area of square = " + this.side * this.side);
+       System.out.println("area of square = " + this.side * this.side);  // area is calculated and printed to the console 
    }
 }
 
 
-public class area{
+public class area{ // main class
   public static void main(String args[]){
-   calculate B = new calculate(5);
-   B.areaOfSquare();
+   calculate B = new calculate(5);// object B of type calculate class is being initialized and perameter is passed 
+
+   B.areaOfSquare();// function to calculate area of square is called using object B of type calculate class
 
   }
 }
